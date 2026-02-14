2980f24b79b1eb05437853601929e1225499703b
Admin <Admin@l.mshome.net>
2026-02-14 11:29:41 -0500

    Commit1

---
+++ Prueba2/src/pru/Pru.java
@@ -0,0 +1,67 @@
+package pru;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+
+public class Pru {
+
+	public static void main(String[] args) {
+
+		
+		try {
+			
+			System.out.println(File.separator);
+			
+	/*
+		
+for (int i = 0; i < 5000; i++) {
+	
+	
+	
+	escribir("cadena3 "+i);
+	Thread.sleep(100);
+	
+}*/
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+public static void escribir(String texto) throws IOException {
+	 
+		
+		File archivo = new File("C:/Users/Admin/Downloads/PruebaG.txt");
+
+	    if (!archivo.exists()) {
+	        archivo.createNewFile();
+	    }
+
+	    FileOutputStream fos = null;
+	    FileChannel canal = null;
+	    FileLock lock = null;
+
+	    try {
+	    	
+	        fos = new FileOutputStream(archivo, true);
+	        canal = fos.getChannel();
+	        lock = canal.lock();
+
+	        fos.write((texto+System.lineSeparator()).getBytes());
+	        fos.flush();
+
+	    } catch (IOException e) {
+	        System.err.println("Error escribiendo archivo: " + e.getMessage());
+	        throw e; // <<--- vuelve a lanzar la excepción
+	    } finally {
+	        if (lock != null) lock.release();
+	        if (canal != null) canal.close();
+	        if (fos != null) fos.close();
+	    }
+	}
+
+}
